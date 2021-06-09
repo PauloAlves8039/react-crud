cdd83a0dc04a50964cd65306b4aec010cf3908af
PauloAlbes8039 <pj38alves@gmail.com>
2021-06-09 10:44:46 -0300

    Implementando os componentes Main e Home

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default (props) => (
+  <Main icon="home" title="Início" subtitle="Projeto CRUD em React.">
+    <div className="display-4">Bem Vindo(a)!</div>
+    <hr />
+    <p>
+      Sistema para exemplificar o desenvolvimento de um CRUD simples com React!
+    </p>
+  </Main>
+);
