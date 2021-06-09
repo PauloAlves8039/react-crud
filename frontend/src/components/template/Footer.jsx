778e7ce4cca56d4ffdb6d969fedf91f2017afedb
PauloAlbes8039 <pj38alves@gmail.com>
2021-06-09 10:08:48 -0300

    Implementando o componente Footer

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -1,4 +1,9 @@
 import "./Footer.css";
 import React from "react";
 
-export default (props) => <footer className="footer">Rodapé</footer>;
+export default (props) => (
+  <footer className="footer">
+    Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+    <strong> Cod<span className="text-danger">3</span>r</strong>
+  </footer>
+);
