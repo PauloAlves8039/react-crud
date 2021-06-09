8423be9320d9ef70eeae4173fec1446286861680
PauloAlbes8039 <pj38alves@gmail.com>
2021-06-09 09:41:08 -0300

    Implementando o componente Logo

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,4 +1,11 @@
 import "./Logo.css";
+import logo from "../../assets/imagens/logo.png";
 import React from "react";
 
-export default (props) => <aside className="Logo">Logo</aside>;
+export default (props) => (
+  <aside className="logo">
+    <a href="/" className="logo">
+      <img src={logo} alt="logo" />
+    </a>
+  </aside>
+);
